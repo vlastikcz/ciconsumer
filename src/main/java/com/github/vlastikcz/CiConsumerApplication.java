294206909d0ea4d0d9d4ef24bf00b744f4e15081
Kotas Vlastimil <Vlastimil.Kotas@ysoft.com>
2016-05-09 21:36:02 +0200

    Initial commit

---
+++ src/main/java/com/github/vlastikcz/CiConsumerApplication.java
@@ -0,0 +1,12 @@
+package com.github.vlastikcz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CiConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CiConsumerApplication.class, args);
+	}
+}
